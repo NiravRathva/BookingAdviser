5479dcc2df3932ab0f5cd87dfdbe560f73fda633
NiravRathva <niravrathwa57@gmail.com>
2023-09-07 22:24:13 +0530

    added condition for mapping data

--- booking/src/Components/HotelRating/HotelRating.js
+++ booking/src/Components/HotelRating/HotelRating.js
@@ -7,22 +7,25 @@ const HotelRating = () => {
 
         <div className="hr">
             {loading ? (
-                <h1>Loading</h1>
+                <h3>Loading Please wait</h3>
             ) : (
                 <>
-                    {data.map((item) => (
-                        <div className="hrItem" key={item._id}>
-                            <img     src={item.photos[0]}alt="" className="hrimg" />
-                            <span className="hrName">{item.name}</span>
-                            <span className="hrcity">{item.city}</span>
-                            <span className="hrprice">Starting from ${item.cheapestPrice}</span>
-                            {item.rating &&      <div className="rating">
-                                <button className="ratingbtn">{item.rating}</button>
-                                <span>exellent</span>
-                            </div>}
-                        </div>
-                    ))}
-
+                    {data.length !== 0 && (
+                        data.map((item) => (
+                            <div className="hrItem" key={item._id}>
+                                <img src={item.photos[0]} alt="" className="hrimg" />
+                                <span className="hrName">{item.name}</span>
+                                <span className="hrcity">{item.city}</span>
+                                <span className="hrprice">Starting from ${item.cheapestPrice}</span>
+                                {item.rating && (
+                                    <div className="rating">
+                                        <button className="ratingbtn">{item.rating}</button>
+                                        <span>excellent</span>
+                                    </div>
+                                )}
+                            </div>
+                        ))
+                    )}
                 </>
             )}
         </div>)
